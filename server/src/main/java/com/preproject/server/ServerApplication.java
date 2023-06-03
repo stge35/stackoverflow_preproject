f2a3591650208355174009cb5c1356abfd2f9263
StAGe-Mac <stage-mac@StAGe-Macui-iMac.local>
2023-06-03 20:48:59 +0900

    feat: 의존성 추가 및 Audit 설정

--- server/src/main/java/com/preproject/server/ServerApplication.java
+++ server/src/main/java/com/preproject/server/ServerApplication.java
@@ -2,7 +2,10 @@ package com.preproject.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
+@EnableJpaAuditing  // Auditing 을 사용하기 위해서 설정
 @SpringBootApplication
 public class ServerApplication {
 
