5a29f6d920f2b1f942a0cdfde38a221241fe3c51
StAGe-Mac <stage-mac@StAGe-Macui-iMac.local>
2023-06-04 23:39:10 +0900

    feat: MemberRepository 작성

---
+++ server/src/main/java/com/preproject/server/domain/member/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package com.preproject.server.domain.member.repository;
+
+import com.preproject.server.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+}
---
+++ server/src/main/java/com/preproject/server/domain/member/service/MemberService.java
@@ -0,0 +1,14 @@
+package com.preproject.server.domain.member.service;
+
+import com.preproject.server.domain.member.dto.MemberDto;
+import com.preproject.server.domain.member.entity.Member;
+import com.preproject.server.domain.member.repository.MemberRepository;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Transactional
+@Service
+public class MemberService {
+
+}
